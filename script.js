dad6a5217f143c3b26b8d854d98f2cb1509f890c
Hernán Marrapodi <hernanmarra2002@gmail.com>
2022-05-05 17:45:33 -0300

    Add book-adding functions

--- script.js
+++ script.js
@@ -3,6 +3,18 @@ let myLibrary = [
     title: "The Fellowship of the Ring",
     author: "J. R. R. Tolkien",
     pages: 423,
+    read: true,
+  },
+  {
+    title: "The Two Towers",
+    author: "J. R. R. Tolkien",
+    pages: 352,
+    read: true,
+  },
+  {
+    title: "The Return of the King",
+    author: "J. R. R. Tolkien",
+    pages: 416,
     read: false,
   },
 ];
@@ -16,13 +28,31 @@ class Book {
   }
 }
 
+window.onload = function () {
+  for (let item of myLibrary) {
+    let book = new Book(item.title, item.author, item.pages, item.read);
+    loadBook(book);
+  }
+};
+
 function addBookToLibrary(title, author, pages, read) {
   let book = new Book(title, author, pages, read);
-  let bookContainer = document.querySelector("#book-container");
-  bookContainer.appendChild(book);
+  myLibrary.push(book);
+  loadBook(book);
 }
 
-function loadBooks() {
-  for (let book in myLibrary) {
-  }
+function loadBook(book) {
+  const bookContainer = document.querySelector("#book-container");
+  let bookElement = document.createElement("div");
+  let bookTitle = document.createElement("p");
+  let bookAuthor = document.createElement("p");
+  let bookPages = document.createElement("p");
+  let bookRead = document.createElement("p");
+  bookTitle.textContent = book.title;
+  bookAuthor.textContent = book.author;
+  bookPages.textContent = book.pages;
+  bookRead.textContent = book.read;
+  bookElement.append(bookTitle, bookAuthor, bookPages, bookRead);
+  bookElement.className = book.read ? "book read" : "book not-read";
+  bookContainer.appendChild(bookElement);
 }
