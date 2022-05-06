2c33f84b80718e5e4484cce2c6468cbf53959afe
Hernán Marrapodi <hernanmarra2002@gmail.com>
2022-05-05 23:08:15 -0300

    Give card buttons functionality

--- script.js
+++ script.js
@@ -48,6 +48,16 @@ function loadBook(book) {
   let bookAuthor = document.createElement("p");
   let bookPages = document.createElement("p");
   let bookRead = document.createElement("button");
+  bookRead.addEventListener("click", () => {
+    console.log("a");
+    if (bookRead.parentNode.dataset.read == "true") {
+      bookRead.parentNode.dataset.read = "false";
+      bookRead.textContent = "Not Read";
+    } else {
+      bookRead.parentNode.dataset.read = "true";
+      bookRead.textContent = "Read";
+    }
+  });
   bookTitle.textContent = "Title: " + book.title;
   bookAuthor.textContent = "Author: " + book.author;
   bookPages.textContent = "Pages: " + book.pages;
